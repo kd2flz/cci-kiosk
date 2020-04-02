3f033649581ca50058cdcf8f4f51ab19dab3c35e
David Rhoads <kd2flz@outlook.com>
2020-04-02 15:26:40 -0400

    added configeration files

---
+++ kiosk.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#This file is placed in the directory /opt
+
+xset -dpms
+xset s off
+openbox-session &
+start-pulseaudio-x11
+
+while true; do
+  rm -rf ~/.{config,cache}/google-chrome/
+  chromium-browser --kiosk --no-first-run  'http://bruderhof.com'
+done
