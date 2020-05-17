0db5cb52509a1beeb3cca743157c94594e5b5f3d
David Rhoads <kd2flz@outlook.com>
2020-05-17 18:02:39 -0400

    changed chromium from snap to apt package

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -12,13 +12,11 @@ sudo apt update
 #Upgrade the server
 sudo apt upgrade -y
 
-#Installs snaps so that chromium will work the same on debian and ubuntu
-sudo apt install -y snapd 
 #install x server, openbox display manager, chromium browser (open source version of Chrome), and pulse audio, for sound
 #--no-install-recommends prevents package manager from installing unnecessary packages wanted by the target packages
 sudo apt install -y --no-install-recommends xorg pulseaudio 
 sudo apt install -y --no-install-recommends openbox 
-sudo snap install chromium 
+sudo apt install chromium-browser 
 #Uncommment below to install Cockpit for remote management - note, if you are using a non-ubuntu based system, like Debian, you will have to change this command
 sudo apt install -y cockpit 
 
