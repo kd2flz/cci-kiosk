51a25a940d39f01f29484c9043d1cebe5b650d67
David Rhoads <kd2flz@outlook.com>
2020-05-25 16:53:57 -0400

    addedd vm_to_disk

---
+++ vm_to_disk.sh
@@ -0,0 +1,15 @@
+#####################################
+# Copy Qemu Image to Physical Disk  #
+#                                   #
+# David Rhoads                      #
+# May 25, 2020                      #
+#####################################
+
+#!/bin/bash
+
+#You will need to set /dev/sbX to the correct disk number (eg sdb or sdc)
+echo This program writes a virtual disk image to a physical drive
+echo CAUTION it will erase the contents of that drive
+echo enter the path to the drive (eg /dev/sdX)
+read vardrive
+qemu-img convert -O raw diskimage.qcow2 $vardrive
