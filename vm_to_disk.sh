c1fb91138b162620ae02dfd0e86fea7f9ca830a7
Ubuntu <ubuntu@ip-172-31-88-98.ec2.internal>
2020-05-26 14:20:47 +0000

    modified:   vm_to_disk.sh

--- vm_to_disk.sh
+++ vm_to_disk.sh
@@ -10,6 +10,6 @@
 #You will need to set /dev/sbX to the correct disk number (eg sdb or sdc)
 echo This program writes a virtual disk image to a physical drive
 echo CAUTION it will erase the contents of that drive
-echo enter the path to the drive (eg /dev/sdX)
+echo enter the path to the drive eg /dev/sdX
 read vardrive
 qemu-img convert -O raw diskimage.qcow2 $vardrive
