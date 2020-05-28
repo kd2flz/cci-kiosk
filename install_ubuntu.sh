a3dfc3a9c336a9a9ecd1bd8dc216c951ebcd41a9
David Rhoads <kd2flz@outlook.com>
2020-05-28 13:32:34 -0400

    updated network configuration

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -52,6 +52,20 @@ sudo cp timesyncd.conf /etc/systemd/timesyncd.conf
 sudo timedatectl set-ntp false
 sudo timedatectl set-ntp on
 
+#Allow network to be configured by network manager
+#Thanks to Martin Wimpress for this bit of code
+  echo "[+] Will now configure network"
+  # Disable cloud-init from managing the network
+  echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+
+  # Instruct netplan to hand all network management to NetworkManager
+  cat <<EOM > /etc/netplan/01-network-manager-all.yaml
+# Let NetworkManager manage all devices on this system
+network:
+  version: 2
+  renderer: NetworkManager
+EOM
+
 #Change grub options
 #Use this option with caution IT MAY BREAK YOUR BOOTLOADER
 #Uncomment below to disable text on screen at boot
