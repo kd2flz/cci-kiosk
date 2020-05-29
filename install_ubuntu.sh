ac0dab84b23630f1a28af3d0f30586f71b8dc75a
David Rhoads <kd2flz@outlook.com>
2020-05-28 20:34:54 -0400

    added sudo to network conf

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -59,10 +59,10 @@ sudo timedatectl set-ntp on
 #Thanks to Martin Wimpress for this bit of code
   echo "[+] Will now configure network"
   # Disable cloud-init from managing the network
-  echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
+  sudo echo "network: {config: disabled}" > /etc/cloud/cloud.cfg.d/99-disable-network-config.cfg
 
   # Instruct netplan to hand all network management to NetworkManager
-  cat <<EOM > /etc/netplan/01-network-manager-all.yaml
+  sudo cat <<EOM > /etc/netplan/01-network-manager-all.yaml
 # Let NetworkManager manage all devices on this system
 network:
   version: 2
