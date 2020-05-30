f2cadcbdd39250cb434960b978a6e2e66d76e464
David Rhoads <kd2flz@outlook.com>
2020-05-30 15:31:14 -0400

    updated network_config

--- network_config.sh
+++ network_config.sh
@@ -17,4 +17,4 @@ EOM
   if [ -e /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf ]; then
     sed -i 's/wifi.powersave = 3/wifi.powersave = 2/' /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
   fi
-}
+
